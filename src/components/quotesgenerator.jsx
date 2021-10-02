bc97a892fd777033c706bd4d69736d1a1d47e5cc
mariomans <mariomans89@gmail.com>
2021-10-02 15:53:48 +0300

    add random quote component

---
+++ src/components/quotesgenerator.jsx
@@ -0,0 +1,71 @@
+import React, {useState} from 'react'
+import data from './data/quotes.json'
+
+const Data = data.quotes
+const nbr = Math.floor(Math.random()*100)
+
+export default function Quotes () {
+    let initialState= {
+        quote: Data[nbr].quote, 
+        author: Data[nbr].author
+    }
+    const [state, setstate] = useState(initialState)
+    const generateColor = () => {
+  
+        let randomColor = (Math.floor(Math.random()*0xFFFFFF)).toString(16);
+        // document.body.style.backgroundColor = "#" + randomColor;
+        // let text = document.querySelector('#code');
+        // text.innerText = "#" + randomColor;
+        return randomColor
+      
+    }
+
+
+    const handleClick = (e) => {
+        const anotherQuote = Math.floor(Math.random()*100);
+        setstate({
+            quote: Data[anotherQuote].quote, 
+            author: Data[anotherQuote].author
+        })
+        const bg = generateColor();
+        const color = generateColor() === bg ? generateColor() : generateColor();
+        let btnChange = document.getElementsByClassName('btn-change')
+        for(let i = 0; i < btnChange.length; i++){
+            btnChange[i].style = `background-color: #${bg}; color: #${color}`;
+        }
+        document.body.style = `background-color: #${bg}; color: #${color}`
+        console.log(bg, color)
+    }
+
+    return <div className={`container-fluid`}>
+        <div className={`container card my-5 w-50 h-75 py-5`}>
+            <h2 className='text-center'>
+               "{state.quote}"           
+            </h2>
+            <p className='text-end'>
+               {state.author}
+            </p>
+            <div className="row">
+                <div className="col-lg-4">
+                    <div className="row">
+                    <div className="col-lg-3">
+                        <button className="btn btn-change btn-primary">
+                <i className="fab fa-twitter"></i>
+            </button>
+                    </div>
+                    <div className="col-lg-4 offset-3">
+                    <button className="btn btn-change btn-primary">
+                    <i className="fab fa-linkedin"></i>
+            </button>
+                    </div>
+                    </div>
+                </div>
+            <div className="col-lg-4 offset-4">
+            <button className="btn btn-change btn-primary" onClick={handleClick}>
+                change
+            </button>
+            </div>
+            </div>
+        </div>
+    </div>
+}
